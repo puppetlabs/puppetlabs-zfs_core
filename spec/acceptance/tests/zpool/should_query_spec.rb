9c30c475faabedaca7261eaf871889e87c3c8a33
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-01-12 23:58:16 -0600

    support acceptance tests on ubuntu

--- spec/acceptance/tests/zpool/should_query_spec.rb
+++ spec/acceptance/tests/zpool/should_query_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper_acceptance'
 RSpec.context 'ZPool: Should Query' do
   before(:all) do
     # ZPool: setup
-    solaris_agents.each do |agent|
+    agents.each do |agent|
       zpool_setup agent
     end
   end
 
   after(:all) do
     # ZPool: cleanup
-    solaris_agents.each do |agent|
+    agents.each do |agent|
       zpool_clean agent
     end
   end
 
-  solaris_agents.each do |agent|
+  agents.each do |agent|
     it 'queries both manages and unmanages zpool resources' do
       # ZPool: ensure create
       apply_manifest_on(agent, "zpool{ tstpool: ensure=>present, disk=>'/ztstpool/dsk1' }") do
